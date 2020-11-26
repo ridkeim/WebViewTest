b3a648e64c6800514be21667e02fb354de3a01d0
ridkeim <ridkeim@gmail.com>
2020-11-26 18:09:19 +0300

    Добавлен способ запуска через Intent.createChooser

--- app/src/main/java/ru/ridkeim/webviewtest/MainActivity.kt
+++ app/src/main/java/ru/ridkeim/webviewtest/MainActivity.kt
@@ -15,11 +15,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button = findViewById<View>(R.id.button)
+        val button2 = findViewById<View>(R.id.button2)
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://ya.ru"))
+        val chooserIntent = Intent.createChooser(intent, "Open with...")
         button.setOnClickListener{
             if(intent.resolveActivity(packageManager) != null ){
                 startActivity(intent)
             }
+        }
+        button2.setOnClickListener{
+            startActivity(chooserIntent)
         }
     }
 }
